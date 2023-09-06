efecf7b95a9667c867ca88d519f78f333474722e
dzmitry_sidorov <jimmaggot@mail.ru>
2023-09-06 09:21:51 +0300

    Add protected endopoints

---
+++ lib/x_budget_backend_web/auth/pipeline.ex
@@ -0,0 +1,10 @@
+defmodule XBudgetBackendWeb.Auth.Pipeline do
+  use Guardian.Plug.Pipeline, otp_app: :x_budget_backend,
+  module: XBudgetBackendWeb.Auth.Guardian,
+  error_handler: XBudgetBackendWeb.Auth.GuardianErrorHandler
+
+  plug Guardian.Plug.VerifySession
+  plug Guardian.Plug.VerifyHeader
+  plug Guardian.Plug.EnsureAuthenticated
+  plug Guardian.Plug.LoadResource
+end
