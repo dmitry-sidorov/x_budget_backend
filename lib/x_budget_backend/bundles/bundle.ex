a12d4da536be5c0f6c0e576e317a8d5ba878f888
dzmitry_sidorov <jimmaggot@mail.ru>
2023-11-13 11:44:05 +0300

    Update fields for bundle model

--- lib/x_budget_backend/bundles/bundle.ex
+++ lib/x_budget_backend/bundles/bundle.ex
@@ -3,8 +3,8 @@ defmodule XBudgetBackend.Bundles.Bundle do
   import Ecto.Changeset
 
   schema "bundles" do
-    field :summary_amount, :float
-    field :amount_updated_at, :naive_datetime
+    field :title, :string
+    field :description, :string
     field :percentage, :float
     belongs_to :group, XBudgetBackend.Groups.Group
     has_many :category, XBudgetBackend.Categories.Category
@@ -15,7 +15,7 @@ defmodule XBudgetBackend.Bundles.Bundle do
   @doc false
   def changeset(bundle, attrs) do
     bundle
-    |> cast(attrs, [:summary_amount, :amount_updated_at, :percentage])
-    |> validate_required([:percentage])
+    |> cast(attrs, [:title, :description, :percentage])
+    |> validate_required([:title, :percentage])
   end
 end
